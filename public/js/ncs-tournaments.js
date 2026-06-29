70c2258685ee265e8361ae4d8158229a499eebae
Jeremiah Cargill <49728959+jeremiah9980@users.noreply.github.com>
2026-06-28 19:24:55 -0500

    Add frontend NCS tournament index

---
+++ public/js/ncs-tournaments.js
@@ -0,0 +1,23 @@
+window.TEXAS_VENOM_NCS_INDEX = {
+  generated_at: "2026-06-28T00:00:00Z",
+  source: "NCS Fastpitch team pages",
+  tracked_teams_only: true,
+  teams: {
+    "12u": {
+      label: "Texas Venom 12U",
+      ncs_team_id: "27-87660",
+      division: "12 & Under Division C",
+      ncs_url: "https://www.playncs.com/fastpitch/Teams/Details/87660/texas-venom-12u",
+      data_path: "/data/ncs-tournaments-12u.json",
+      tracker_page: "/tournament-tracker-12u.html"
+    },
+    "14u": {
+      label: "Texas Venom 14U",
+      ncs_team_id: "27-87549",
+      division: "14 & Under Division C",
+      ncs_url: "https://www.playncs.com/fastpitch/Teams/Details/87549/texas-venom-14u",
+      data_path: "/data/ncs-tournaments-14u.json",
+      tracker_page: "/tournament-tracker-14u.html"
+    }
+  }
+};
